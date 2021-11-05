ca4d035a2c9af237afef633c9bcc94cfa0c3a404
kumarjangkhng1 <93480518+kumarjangkhng1@users.noreply.github.com>
2021-11-05 21:08:13 +0530

    Update machine.js

--- machine.js
+++ machine.js
@@ -16,12 +16,29 @@
 			alert('Enter Please Lover Name');
 			return;
 		}
-		var resultsh = document.getElementById('resultsh');
-		resultsh.style['font-size']='40px';
-	    resultsh.value = Math.floor(Math.random()*100) + "%";
+		var resultsh = document.getElementById('futureper').innerHTML = Math.floor(Math.random()*100);
+		var resultsh1 = document.getElementById('futureper1').innerHTML = Math.floor(Math.random()*100) ;
+		// resultsh.style['font-size']='40px';
+
+		document.getElementById('resultsh').value=Number(resultsh)+Number(resultsh1) +'%';
 		document.getElementById('notify').style['display']='block';
 		console.log('hellow');
 	}
 
 
+}
+
+var n = localStorage.getItem('on_load_counter');
+
+if (n === null) {
+  n = 0;
+}
+n++;
+
+localStorage.setItem("on_load_counter", n);
+
+nums = n.toString().split('').map(Number);
+document.getElementById('CounterVisitor').innerHTML = '';
+for (var i of nums) {
+  document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
 }
