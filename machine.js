5194316f29d2e52a4cc48831e52b68b34889987e
kumarjangkhng1 <93480518+kumarjangkhng1@users.noreply.github.com>
2021-11-06 11:05:51 +0530

    Update machine.js

--- machine.js
+++ machine.js
@@ -22,23 +22,13 @@
 
 		document.getElementById('resultsh').value=Number(resultsh)+Number(resultsh1) +'%';
 		document.getElementById('notify').style['display']='block';
+		document.getElementById('refresh').style['display']='block';
 		console.log('hellow');
 	}
 
 
 }
 
-var n = localStorage.getItem('on_load_counter');
-
-if (n === null) {
-  n = 0;
-}
-n++;
-
-localStorage.setItem("on_load_counter", n);
-
-nums = n.toString().split('').map(Number);
-document.getElementById('CounterVisitor').innerHTML = '';
-for (var i of nums) {
-  document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
+function refresh(){
+	location.reload();
 }
